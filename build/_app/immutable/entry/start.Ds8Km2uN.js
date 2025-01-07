a6c3a2e10ff79d674359b175ae2934400112e5c8
Pedram Amani <amanipedram@gmail.com>
2025-01-07 09:30:13 -0800

    v1

---
+++ build/_app/immutable/entry/start.Ds8Km2uN.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.B-3cpeGF.js";export{t as start};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import '../app.css'
+</script>
+
+<svelte:head>
+	<meta name="title" content="Epitora: Software tailor-made for your business" />
+</svelte:head>
+
+<div class="fixed inset-0 overflow-hidden bg-white dark:bg-black">
+	<img class="absolute left-6 top-6 h-10 w-auto" src="full.png" alt="Epitora" />
+	<div class="mx-auto mt-46 px-4 sm:mt-64 flex flex-col items-center max-w-3xl">
+		<h1 class="text-5xl sm:text-7xl font-semibold tracking-tight text-center text-balance text-black/90 dark:text-white/95">
+			Software tailor-made for your business
+		</h1>
+		<p class="mt-8 text-lg sm:text-xl/8 font-medium text-center text-pretty text-black/60 dark:text-white/80">
+			Your business is unique, your software stack should be too. We develop and integrate solutions specific to your needs.
+		</p>
+		<a
+			href="mailto:inquiry@epitora.com?subject=Website%20inquiry&body=Hi%20Epitora%20team,%0A%0AWe%20are%20a%20small%20business%20looking%20for"
+			class="mt-10 rounded-md bg-indigo-600 hover:bg-indigo-500 px-3.5 py-2.5 font-semibold text-white shadow-xs"
+		>
+			Get in touch
+		</a>
+	</div>
+
+	<div class="absolute inset-0 -z-10 transform-gpu overflow-hidden blur-3xl">
+		<div
+			class="absolute -top-20 left-0 w-lg sm:w-2xl aspect-1155/678 rotate-30 bg-yellow-500 opacity-35"
+			style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+		></div>
+		<div
+			class="absolute bottom-0 right-0 w-lg sm:w-2xl aspect-1155/678 -rotate-10 bg-indigo-500 opacity-35"
+			style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+		></div>
+	</div>
+</div>
